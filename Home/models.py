d18be8d4bf4c9c53f2610b5f3c260cb8d5982196
keerthana <keerthanadinesha2023b@mca.ajce.in>
2023-04-27 15:33:32 +0530

    seminar

--- Home/models.py
+++ Home/models.py
@@ -137,29 +137,51 @@ class scheduleingday(models.Model):
     day = models.CharField(max_length=100)
     def __str__(self):
         return self.day
-
-
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 
-
-
 class Bins(models.Model):
     Bin_id = models.AutoField(primary_key=True)
-    Bin_name = models.CharField(max_length=100)
+    Bin_name = models.CharField(max_length=100, unique=True)
     Bin_color = models.ForeignKey(bin_color, verbose_name='bin_color', on_delete=models.DO_NOTHING, default="")
     Bin_location = models.ForeignKey(location, verbose_name='region', on_delete=models.DO_NOTHING, default="")
-    Bin_address1 = models.CharField(max_length=100)
-    Bin_address2 = models.CharField(max_length=100)
-    Bin_address3 = models.CharField(max_length=100)
-    pincode = models.BigIntegerField()
-    distance_KM = models.CharField(max_length=100)
-    total_time = models.TimeField(default=0)
-    Bin_date= models.DateField(default=0)
-    Bin_status = models.CharField(max_length=50)
+    Bin_address1 = models.CharField(max_length=100, validators=[RegexValidator(regex='^[\w\-\. ]+$', message='Invalid address.')])
+    Bin_address2 = models.CharField(max_length=100, validators=[RegexValidator(regex='^[\w\-\. ]+$', message='Invalid address.')])
+    Bin_address3 = models.CharField(max_length=100, validators=[RegexValidator(regex='^[\w\-\. ]+$', message='Invalid address.')])
+    pincode = models.BigIntegerField(validators=[RegexValidator(regex='^[0-9]{6}$', message='Invalid pincode.')])
+    distance_KM = models.DecimalField(max_digits=5, decimal_places=2, validators=[RegexValidator(regex='^\d+(\.\d{1,2})?$', message='Invalid distance.')])
+    total_time = models.TimeField(default='00:00:00')
+    Bin_date= models.DateField()
+    Bin_status = models.CharField(max_length=50, choices=[('Active', 'Active'), ('Inactive', 'Inactive')])
     collections_day = models.ForeignKey(scheduleingday,verbose_name='day',on_delete=models.DO_NOTHING,default="")
+
     def __str__(self):
-            return self.Bin_name
+        return self.Bin_name
+
+    def clean(self):
+        super().clean()
+        errors = {}
+        if not self.Bin_date:
+            errors['Bin_date'] = ValidationError('Invalid date.')
+        if errors:
+            raise ValidationError(errors)
+
+# class Bins(models.Model):
+#     Bin_id = models.AutoField(primary_key=True)
+#     Bin_name = models.CharField(max_length=100)
+#     Bin_color = models.ForeignKey(bin_color, verbose_name='bin_color', on_delete=models.DO_NOTHING, default="")
+#     Bin_location = models.ForeignKey(location, verbose_name='region', on_delete=models.DO_NOTHING, default="")
+#     Bin_address1 = models.CharField(max_length=100)
+#     Bin_address2 = models.CharField(max_length=100)
+#     Bin_address3 = models.CharField(max_length=100)
+#     pincode = models.BigIntegerField()
+#     distance_KM = models.CharField(max_length=100)
+#     total_time = models.TimeField(default=0)
+#     Bin_date= models.DateField(default=0)
+#     Bin_status = models.CharField(max_length=50)
+#     collections_day = models.ForeignKey(scheduleingday,verbose_name='day',on_delete=models.DO_NOTHING,default="")
+#     def __str__(self):
+#             return self.Bin_name
 
 class Driver(models.Model):
     driver_id = models.AutoField(primary_key=True)
@@ -206,16 +228,36 @@ class complaintpost(models.Model):
     def __str__(self):
         return self.c_landmark
 
+from django.core.exceptions import ValidationError
 
 class workupdation(models.Model):
     work_id = models.AutoField(primary_key=True)
-    Bin_id = models.ForeignKey(Bins,verbose_name='Bin_name',on_delete=models.DO_NOTHING,default="")
-    Location = models.ForeignKey(location,verbose_name='region',on_delete=models.DO_NOTHING,default="")
-    # Area = models.ForeignKey(Bins,verbose_name='Bin_address1',on_delete=models.DO_NOTHING,default="")
-    Time = models.TimeField(default=0)
-    Date = models.DateField(default=0)
-    status = models.CharField(max_length=100)
-    Driver_name = models.ForeignKey(Driver,verbose_name='driver_name',on_delete=models.DO_NOTHING,default="")
+    Bin_id = models.ForeignKey(Bins, verbose_name='Bin_name', on_delete=models.DO_NOTHING, default="")
+    Location = models.ForeignKey(location, verbose_name='region', on_delete=models.DO_NOTHING, default="")
+    Time = models.TimeField(default='00:00:00')
+    Date = models.DateField()
+    status = models.CharField(max_length=100, choices=[('Pending', 'Pending'), ('In progress', 'In progress'), ('Completed', 'Completed')])
+    Driver_name = models.ForeignKey(Driver, verbose_name='driver_name', on_delete=models.DO_NOTHING, default="")
+
+    def clean(self):
+        super().clean()
+        errors = {}
+        if not self.Date:
+            errors['Date'] = ValidationError('Invalid date.')
+        if errors:
+            raise ValidationError(errors)
+
+    def __str__(self):
+        return str(self.work_id)
+
+# class workupdation(models.Model):
+#     work_id = models.AutoField(primary_key=True)
+#     Bin_id = models.ForeignKey(Bins,verbose_name='Bin_name',on_delete=models.DO_NOTHING,default="")
+#     Location = models.ForeignKey(location,verbose_name='region',on_delete=models.DO_NOTHING,default="")
+#     Time = models.TimeField(default=0)
+#     Date = models.DateField(default=0)
+#     status = models.CharField(max_length=100)
+#     Driver_name = models.ForeignKey(Driver,verbose_name='driver_name',on_delete=models.DO_NOTHING,default="")
 
 
 
